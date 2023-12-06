819829158168a67f5ebb877c6c356fde9e7c2252
StvKush2403 <kushagrastv2403@gmail.com>
2023-12-06 23:19:01 +0530

    kushagra commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Header from '@/Components/Header'
+import Stack from '@/Components/Stack'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
@@ -114,5 +117,8 @@ export default function Home() {
         </a>
       </div>
     </main>
+    <Header />
+    <Stack />
+    </>
   )
 }
